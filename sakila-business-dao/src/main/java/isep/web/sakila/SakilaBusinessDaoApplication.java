adc842cc2314e95719599e56d0c098b6b17973c3
nicosim <nico93simo@hotmail.fr>
2017-11-28 17:54:14 +0100

    Project JPA

---
+++ sakila-business-dao/src/main/java/isep/web/sakila/SakilaBusinessDaoApplication.java
@@ -0,0 +1,12 @@
+package isep.web.sakila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaBusinessDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaBusinessDaoApplication.class, args);
+	}
+}

7cdedc48d1ead7279694f83a24f456cfac51e634
romnar <romain.narboni@isep.fr>
2017-12-09 13:52:39 +0100

    test user login/password (static security)

---
+++ sakila-business-webapi/src/main/java/isep/web/sakila/webapi/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package isep.web.sakila.webapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	protected void configure(HttpSecurity http) throws Exception {
+	    http
+	            .authorizeRequests().antMatchers("/").permitAll()
+	            .anyRequest().authenticated()
+	            .and()
+	            .formLogin()
+	            .and()
+	            .httpBasic();
+	}
+	
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user1").password("azerty").roles("USER").and()
+                .withUser("user2").password("qwerty").roles("USER");
+    }
+}
+
+
